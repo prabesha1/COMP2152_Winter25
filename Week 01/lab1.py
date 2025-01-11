1797e3558c027dfc8f77a2738c31c4862fa4d530
prabesha1 <prabesha1@outlook.com>
2025-01-11 14:18:08 -0500

    first commit

---
+++ Week 01/lab1.py
@@ -0,0 +1,22 @@
+# Sample Coding Questions 01 Week 01
+# Prabesh Shrestha
+
+# Defining Variables
+array_variable = [1, 4, 7, 9]
+
+# Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+# Fully-Bracketed Version
+e = (a - ((b ** c) // d) + (a % c))
+
+# Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common Functions
+userAge = int(input("Enter your age: "))
+userAge += 22
+print(f"Now showing the shop items filtered by age: {userAge}")
