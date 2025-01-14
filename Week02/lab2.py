9a5ea3f38a8832bc944e5c4db8415468882a2524
prabesha1 <prabesha1@outlook.com>
2025-01-14 10:36:04 -0500

    add func with user input and type conversion

---
+++ Week02/lab2.py
@@ -0,0 +1,18 @@
+elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "carbon"]
+print("Elements: ", elements)
+#  git add . && git commit -m "add elements array" && git push
+
+# def funct_name():
+#     return True
+# def say_greeting(name, message="hi"):
+#     print(f" {message}, {name}")
+# say_greeting("Maziar")
+# say_greeting("Maziar", "Hello")
+
+def get_valid_int_input(prompt):
+    while True:
+        try:
+            return int(input(print))
+        except ValueError:
+            print("Error: Please enter a valid integer!")
+            continue
